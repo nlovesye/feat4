ef7b49e9f1cead04dd0c23eaf0b7cb74c7285264
nloves <894765963@qq.com>
2020-09-18 17:27:37 +0800

    Create App.vue

---
+++ web/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="app">
+        <router-view />
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="css">
+html, body, .app {
+    width: 100%;
+    height: 100%;
+}
+.clear{
+    zoom: 1;
+}
+.clear:after {
+    clear: both;
+    content: '';
+    display: block;
+    width: 0;
+    height: 0;
+    visibility: hidden;
+}
+</style>
