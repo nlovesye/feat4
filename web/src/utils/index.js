915e24b804e20f217042fdf31be03ef9ffce6d20
nloves <894765963@qq.com>
2020-09-18 17:29:42 +0800

    Create index.js

---
+++ web/src/utils/index.js
@@ -0,0 +1,11 @@
+export {
+    default as axiosInstance,
+    apiGet,
+    apiPost,
+    apiDelete,
+    apiPut
+} from './request'
+
+export { grade, getLocal, setLocal, removeLocal } from './util'
+
+export * as dataDic from './dataDic'
