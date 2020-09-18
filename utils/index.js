d3f1dd6b9091eacaac812a54d0f248accd6f294a
nloves <894765963@qq.com>
2020-09-18 17:04:57 +0800

    Create index.js

---
+++ utils/index.js
@@ -0,0 +1,12 @@
+const db = require('./db')
+const util = require('./util')
+const res = require('./res')
+const apiAuth = require('./apiAuth')
+
+exports.util = util
+
+exports.db = db
+
+exports.res = res
+
+exports.apiAuth = apiAuth
