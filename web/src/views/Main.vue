a0b29742e62034897f13c56a28fcbfdb43146726
nloves <894765963@qq.com>
2020-09-18 17:28:26 +0800

    Create Main.vue

---
+++ web/src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="p_home">
+        <the-header />
+        <router-view />
+    </div>
+</template>
+
+<script>
+import TheHeader from '@/components/Header'
+export default {
+    name: 'Main',
+    components: {
+        TheHeader
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.p_home {
+    padding-top: 42px;
+}
+</style>
